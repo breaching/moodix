3cd4776ca5e4e9aa82aa553c38ef41fe64ba44f6
unknown <alexis@dubus.pro>
2026-01-08 23:58:31 +0100

    fix(deploy): add /dist/ to base path for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/journal-tcc/',
+  base: '/journal-tcc/dist/',
   plugins: [react()],
   resolve: {
     alias: {
